00dff4fce7cd85dfd1f9c6516d4cd8c0e0334caf
AutoCAR Agent <autocar-agent@autopath.dev>
2026-02-10 06:19:04 +0000

    AutoCAR Agent: REN-76 - Design Modern Contact Us Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Banner from './components/Banner';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactUsPage from './pages/ContactUsPage';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav className="bg-gray-800 p-4">
-          <ul className="flex space-x-4">
-            <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
-            <li><Link to="/contact" className="text-white hover:text-gray-300">Contact Us</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Banner />} />
-          <Route path="/contact" element={<ContactUsPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/contact" element={<ContactUsPage />} />
+        {/* Add other routes here */}
+      </Routes>
     </Router>
   );
 }
